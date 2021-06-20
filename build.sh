0b6828c101a33f25582c4198b72d61fbcfef9c61
BuildTools <unconfigured@null.spigotmc.org>
2021-06-19 20:17:46 -0400

    better build

--- build.sh
+++ build.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-sudo docker build . --no-cache -t mc && sudo docker-compose up -d --build
+MCROOT=/home/minecraft/mc-root
+
+if [ ! -d "/home/minecraft/paper-mc-docker" ]; then
+	cd /home/minecraft/
+	git clone https://github.com/brandon-gayne/paper-mc-docker.git
+	echo "Added paper build repo"
+fi
+
+cd /home/minecraft
+
+if [ ! -f "/home/minecraft/paper-mc-docker/.env" ]; then
+	echo "MCROOT=/home/minecraft/paper-mc-docker" >> paper-mc-docker/.env
+	echo "Added environment file"
+fi
+
+echo "Checking config and world files exist!"
+
+for dir in world world_nether world_the_end config; do
+	if [ ! -d "$MCROOT/$dir" ]; then
+		mkdir $MCROOT/$dir
+		echo "$dir added!"
+	else
+		echo "$dir exists!"
+	fi
+done
+
+cd paper-mc-docker
+
+echo "Building image!"
+
+docker build . --no-cache -t mc
+
+echo "Latest image built!"
+
+docker-compose up -d
