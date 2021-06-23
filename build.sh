9098a9333b26a036c7ce0cca480d0a7fe87ba670
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 22:33:08 -0400

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 MCROOT=/home/minecraft/mc-root
+DSHOOK=$(cat /home/minecraft/.discord/.env)
+
+if [! command -v discord.sh &> /dev/null ]; then
+	git clone https://github.com/ChaoticWeg/discord.sh
+	cd discord.sh
+	mv discord.sh /usr/bin/discord.sh
+fi
 
 if [ ! -d "/home/minecraft/paper-mc-docker" ]; then
 	cd /home/minecraft/
@@ -15,6 +22,8 @@ if [ ! -f "/home/minecraft/paper-mc-docker/.env" ]; then
 	echo "Added environment file"
 fi
 
+
+
 echo "Checking config and world files exist!"
 
 for dir in world world_nether world_the_end config; do
@@ -26,12 +35,24 @@ for dir in world world_nether world_the_end config; do
 	fi
 done
 
+
+function discord_message() {
+	discord.sh --webhook-url=$DSHOOK --username "MC Updater Bot" --text "$1"
+}
+
+discord_message "Server going down for updates!"
+
 cd paper-mc-docker
 
-echo "Building image!"
+discord_message "Building new server image!"
 
 docker build . --no-cache -t mc
 
-echo "Latest image built!"
+discord_message "Latest image built!"
 
-docker-compose up -d
+major=`curl https://papermc.io/api/v2/projects/paper | jq  -r '.version_groups[-1]'`
+build=`curl https://papermc.io/api/v2/projects/paper/version_group/$major/builds | jq -r '.builds[-1].build'` 
+
+discord_message "Server rebuilt with build #$build on Minecraft version $major"
+
+docker-compose up -d 
